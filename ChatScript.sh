d55cb8db707572e59a62266abf7775fe4d831b50
Christian Antony Quero <christianantonyquero@gmail.com>
2020-05-30 06:52:40 +0800

    Adding chat server and client. Changes in terminal multiplexer and bash scripts

---
+++ ChatScript.sh
@@ -0,0 +1,8 @@
+sleep 3s
+tmux send-keys -t 1 'Hi' Enter
+sleep 1s
+tmux send-keys -t 2 'Hello' Enter
+sleep 1s
+tmux send-keys -t 1 'What is your name?' Enter 
+sleep 1s
+tmux send-keys -t 2 'My name is Christian Antony Quero' Enter
